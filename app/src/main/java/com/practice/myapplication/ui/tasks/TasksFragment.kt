b5ab2931031c7710b654ab3916ff1b9f261777a5
sarjshah <sarju@novoda.com>
2021-12-02 17:58:38 +0000

    Add navigation component

---
+++ app/src/main/java/com/practice/myapplication/ui/tasks/TasksFragment.kt
@@ -0,0 +1,8 @@
+package com.practice.myapplication.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.practice.myapplication.R
+
+class TasksFragment: Fragment(R.layout.fragment_tasks) {
+
+}
