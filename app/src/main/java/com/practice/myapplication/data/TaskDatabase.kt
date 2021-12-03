e9d3e62a720c3becc3ffff577b8412420903569d
sarjshah <sarju@novoda.com>
2021-12-03 10:23:42 +0000

    Add callback to populate database with test data

--- app/src/main/java/com/practice/myapplication/data/TaskDatabase.kt
+++ app/src/main/java/com/practice/myapplication/data/TaskDatabase.kt
@@ -2,10 +2,36 @@ package com.practice.myapplication.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.practice.myapplication.di.ApplicationScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+import javax.inject.Provider
 
 @Database(entities = [Task::class], version = 1)
-abstract class TaskDatabase: RoomDatabase() {
+abstract class TaskDatabase : RoomDatabase() {
 
     abstract fun taskDao(): TaskDao
 
+    class Callback @Inject constructor(
+        private val database: Provider<TaskDatabase>,
+        @ApplicationScope private val applicationScope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+
+            val dao = database.get().taskDao()
+            applicationScope.launch {
+                dao.insert(Task(name = "Wash the dishes"))
+                dao.insert(Task(name = "Do the laundry"))
+                dao.insert(Task(name = "Buy groceries", isImportant = true))
+                dao.insert(Task(name = "Go for a walk"))
+                dao.insert(Task(name = "Call dad", isCompleted = true))
+                dao.insert(Task(name = "Go to gym"))
+                dao.insert(Task(name = "Prepare the bins"))
+                dao.insert(Task(name = "Prepare food", isCompleted = true))
+            }
+        }
+    }
 }
--- app/src/main/java/com/practice/myapplication/di/AppModule.kt
+++ app/src/main/java/com/practice/myapplication/di/AppModule.kt
@@ -8,6 +8,9 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Qualifier
 import javax.inject.Singleton
 
 @Module
@@ -16,12 +19,21 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideDatabase(app: Application): TaskDatabase =
+    fun provideDatabase(app: Application, callback: TaskDatabase.Callback): TaskDatabase =
         Room.databaseBuilder(app, TaskDatabase::class.java, "task_database")
             .fallbackToDestructiveMigration()
+            .addCallback(callback)
             .build()
-
 
     @Provides
     fun provideTaskDao(db: TaskDatabase): TaskDao = db.taskDao()
+
+    @ApplicationScope
+    @Provides
+    @Singleton
+    fun provideApplicationScope(): CoroutineScope = CoroutineScope(SupervisorJob())
 }
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class ApplicationScope
